765d8830bac42aa92978cf62d8f9a1f5418603ac
ThomasChen94 <ThomasChen@DN0a249824.SUNet>
2018-02-11 20:21:13 -0800

    correct q4.py

--- hm2/code/q4.py
+++ hm2/code/q4.py
@@ -2,8 +2,22 @@ import numpy as np
 import random
 
 
-def findMostEffectiveFeature(X_dup, X_used_ind, y, residual):
-	return random.randint(1, 3)
+# def findMostEffectiveFeature(X_dup, X_used_ind, y, residual):
+# 	return random.randint(1, 3)
+
+
+def findMostEffectiveFeature(mat, col_idx, y, residual):
+    min_val = float("inf")
+    for i in range(mat.shape[1]):
+    	if i not in col_idx:
+	        vec = mat[:,i]
+	        beta = float(np.dot(vec, y)) / np.dot(vec, vec)
+	        #print vec
+	        val = np.linalg.norm(residual - beta * vec)
+	        if val < min_val:
+	            min_idx = i
+	            min_val = val
+    return min_idx
 
 
 '''
@@ -17,7 +31,7 @@ def forwardStepwiseRegModel(X, y):
 	intercept = np.ones([N, ])
 	X = np.hstack((np.reshape(intercept, [N, -1]), X))
 	beta = np.zeros(p + 1)
-	beta[0] = np.dot(intercept, y) / np.dot(intercept, intercept)
+	beta[0] = np.dot(intercept, y) * 1.0 / np.dot(intercept, intercept)
 
 
 	M = np.zeros([p + 1, p + 1])
@@ -32,14 +46,15 @@ def forwardStepwiseRegModel(X, y):
 	
 		for j in xrange(p):
 			if j not in X_used_ind:
-				X_dup[:, j] -= np.dot(X[:, j], a) / np.dot(a, a) * a
+				X_dup[:, j] -= np.dot(X[:, j], a) * 1.0 / np.dot(a, a) * a
+				print "gamma: ", np.dot(X[:, j], a) * 1.0 / np.dot(a, a) * a
 		residual = y - np.matmul(X, beta)
-		
+		#print residual, "    beta: ", beta
 		# find the element that reduces the residual most 
 		new_pick_ind = findMostEffectiveFeature(X_dup, X_used_ind, y, residual)
-		gamma_list.append(np.dot(X[:, j], a) / np.dot(a, a))
-		zp = X[:, new_pick_ind] # the column we process at this iteration
-		beta[new_pick_ind] = np.dot(zp, y) / np.dot(zp, zp)
+		zp = X_dup[:, new_pick_ind] # the column we process at this iteration
+		print zp, "\n"
+		beta[new_pick_ind] = np.dot(zp, y) * 1.0 / np.dot(zp, zp)
 		X_used_ind.append(new_pick_ind)
 
 
@@ -48,8 +63,8 @@ def forwardStepwiseRegModel(X, y):
 
 
 if __name__ == '__main__':
-	X = np.array([[1,2,2], [3,3,3],[1,1,1]])
-	y = np.array([1, 0.5, 2])
+	X = np.array([[1,0,0], [0,1,0],[0,0,1]])
+	y = np.array([3, 6, 9])
 	print forwardStepwiseRegModel(X, y)
 
 
